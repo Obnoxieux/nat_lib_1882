d80c7fe70f33966eb7e969aa151f14091bdee9d1
David Battefeld <davidbattefeld@gmail.com>
2025-07-21 20:18:02 +0200

    feat: serve HTML or JSON conditionally

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -1,8 +1,10 @@
 package dev.obnx
 
 import dev.obnx.model.BookRepository
+import dev.obnx.model.Page
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.jte.JteContent
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.respond
 import io.ktor.server.routing.*
@@ -18,7 +20,18 @@ fun Application.configureSerialization(repository: BookRepository) {
         route("/books") {
             get {
                 val books = repository.allBooks()
-                call.respond(books)
+
+                val acceptHeader = call.request.headers["Accept"]
+
+                if (acceptHeader?.contains("application/json") == true) {
+                    call.respond(books)
+                } else {
+                    val page = Page(
+                        title = "Catalogue Page",
+                        description = "Browse full 1882 catalogue of books in the Damascus Public Library"
+                    )
+                    call.respond(JteContent("books.kte", mapOf("books" to books, "page" to page)))
+                }
             }
         }
     }
