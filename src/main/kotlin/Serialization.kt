ab1be632d6191c8709c563d3f22e4823220a4aac
David Battefeld <davidbattefeld@gmail.com>
2025-07-22 10:57:31 +0200

    feat: add type-safe books route

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -2,10 +2,12 @@ package dev.obnx
 
 import dev.obnx.model.BookRepository
 import dev.obnx.model.Page
+import dev.obnx.resources.Books
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.jte.JteContent
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.resources.get
 import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 import kotlinx.serialization.ExperimentalSerializationApi
@@ -17,21 +19,19 @@ fun Application.configureSerialization(repository: BookRepository) {
     }
 
     routing {
-        route("/books") {
-            get {
-                val books = repository.allBooks()
+        get<Books> { book ->
+            val books = repository.allBooks()
 
-                val acceptHeader = call.request.headers["Accept"]
+            val acceptHeader = call.request.headers["Accept"]
 
-                if (acceptHeader?.contains("application/json") == true) {
-                    call.respond(books)
-                } else {
-                    val page = Page(
-                        title = "Catalogue Page",
-                        description = "Browse full 1882 catalogue of books in the Damascus Public Library"
-                    )
-                    call.respond(JteContent("books.kte", mapOf("books" to books, "page" to page)))
-                }
+            if (acceptHeader?.contains("application/json") == true) {
+                call.respond(books)
+            } else {
+                val page = Page(
+                    title = "Catalogue Page",
+                    description = "Browse full 1882 catalogue of books in the Damascus Public Library"
+                )
+                call.respond(JteContent("books.kte", mapOf("books" to books, "page" to page)))
             }
         }
     }
---
+++ src/main/kotlin/resources/Books.kt
@@ -0,0 +1,18 @@
+package dev.obnx.resources
+
+import io.ktor.resources.Resource
+
+@Resource("/books")
+class Books(
+    val genre: Long? = null,
+    val endowment: Long? = null,
+    val author: Long? = null,
+    val manuscript: Boolean? = null,
+    val print: Boolean? = null,
+    val limit: Long? = null,
+    val offset: Long? = null,
+    val search: String? = null,
+) {
+    @Resource("{id}")
+    class Id(val parent: Books = Books(), val id: Long)
+}
