e2ebab655cb00a992a0da218e618b2f83957abd4
David Battefeld <post@davidbattefeld.de>
2025-08-26 19:37:14 +0200

    fix: fix middleware handling
    always call next!

--- backend-go/api/middleware.go
+++ backend-go/api/middleware.go
@@ -20,6 +20,8 @@ func CORSHandler() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
+
+			next.ServeHTTP(w, r)
 		})
 	}
 }
