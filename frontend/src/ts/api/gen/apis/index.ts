6d43e8ab155ce8f8a358ce80f16dd3f4ece22a78
David Battefeld <post@davidbattefeld.de>
2025-08-30 11:03:20 +0200

    feat: generate OpenAPI client

---
+++ frontend/src/ts/api/gen/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthorsApi';
+export * from './BooksApi';
+export * from './EndowmentsApi';
+export * from './GenresApi';
---
+++ frontend/src/ts/api/gen/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Author';
+export * from './Book';
+export * from './Endowment';
+export * from './Genre';
+export * from './GetAuthors200Response';
+export * from './GetBooks200Response';
+export * from './GetBooksByAuthor200Response';
+export * from './ModelError';
